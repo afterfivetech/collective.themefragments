24f9b376664d55d7ce4b88375ecb5cb83e02f7ee
Holden Hao <holdenhao@gmail.com>
2020-07-29 15:25:49 +0800

    back to development

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collective.themefragments',
-    version='2.13.1+afive.dev1',
+    version='2.13.1+afive.dev2',
     description='Theme fragments for plone.app.theming',
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGES.rst').read()),
